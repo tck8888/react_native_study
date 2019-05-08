e0115e228e19daaa2eb0e4c99b1daf3f83cf1fdf
tck8888 <380054728@qq.com>
2019-05-09 00:18:29 +0800

    react-navigation添加1

--- HelloReactNative/index.js
+++ HelloReactNative/index.js
@@ -4,6 +4,9 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
+import AppNavigator from "./js/navigator/AppNavigator";
+import WelcomePage from './js/page/WelcomePage';
+import HomePage from './js/page/HomePage';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => WelcomePage);
