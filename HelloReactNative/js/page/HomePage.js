bab34e3dad98be576c5d414a80b7bbde8990ba8c
tck8888 <380054728@qq.com>
2019-05-12 15:10:12 +0800

    处理android的物理返回键

--- HelloReactNative/js/page/HomePage.js
+++ HelloReactNative/js/page/HomePage.js
@@ -1,29 +1,46 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View,} from 'react-native';
+import {StyleSheet, Text, View, BackHandler} from 'react-native';
 
 import {
     createAppContainer,
-    createBottomTabNavigator
+    createBottomTabNavigator,
+    NavigationActions
 } from "react-navigation";
 
-import MyPage from "./MyPage";
-import FavoritePage from "./FavoritePage";
-import PopularPage from "./PopularPage";
-import TrendingPage from "./TrendingPage";
-
-import MaterialIcons from "react-native-vector-icons/MaterialIcons"
-import Ionicons from "react-native-vector-icons/Ionicons"
-import Entypo from "react-native-vector-icons/Entypo"
 import NavigationUtil from "../navigator/NavigationUtil";
 import DynamicTabNavigator from "../navigator/DynamicTabNavigator";
+import {connect} from "react-redux";
 
 type Props = {};
 
 
-export default class HomePage extends Component<Props> {
+class HomePage extends Component<Props> {
+    componentDidMount(): void {
+        BackHandler.addEventListener("hardwareBackPress", this.onBackPress);
+    }
+
+    componentWillUnmount(): void {
+        BackHandler.removeEventListener("hardwareBackPress", this.onBackPress);
+    }
+
+    onBackPress = () => {
+        const {dispatch, nav} = this.props;
+        if (nav.routes[1].index === 0) {
+            return false;
+        }
+        dispatch(NavigationActions.back());
+        return true;
+
+    };
 
     render() {
-        NavigationUtil.navigation=this.props.navigation;
+        NavigationUtil.navigation = this.props.navigation;
         return <DynamicTabNavigator/>
     }
 }
+
+const mapStateToProps = state => ({
+    nav: state.nav
+});
+
+export default connect(mapStateToProps)(HomePage);
